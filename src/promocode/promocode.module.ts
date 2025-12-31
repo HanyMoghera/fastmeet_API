a3b91ff568f16ae62372b0470933d6792782f4e0
Hany Moghera <hmougera@gmail.com>
2025-12-31 19:15:31 +0200

    Promocode CRUD

--- src/promocode/promocode.module.ts
+++ src/promocode/promocode.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PromocodeService } from './promocode.service';
 import { PromocodeController } from './promocode.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Promocode } from './entities/promocode.entity';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([Promocode])
+  ],
   controllers: [PromocodeController],
   providers: [PromocodeService],
 })
--- src/promocode/promocode.service.ts
+++ src/promocode/promocode.service.ts
@@ -1,26 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePromocodeDto } from './dto/create-promocode.dto';
 import { UpdatePromocodeDto } from './dto/update-promocode.dto';
+import { Repository } from 'typeorm';
+import { Promocode } from './entities/promocode.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class PromocodeService {
-  create(createPromocodeDto: CreatePromocodeDto) {
-    return 'This action adds a new promocode';
+
+  constructor(
+    @InjectRepository(Promocode)
+    private readonly repo: Repository<Promocode>
+  ){}
+ async create(createPromocodeDto: CreatePromocodeDto) {
+
+  // get user input data
+  const {
+    code,
+    discount_type,
+    discount_value,
+    valid_from,
+    valid_to,
+    per_user_limit,
+    usage_limit
+  } = createPromocodeDto; 
+
+// cretate it.
+    const promocode = await this.repo.create({
+    code,
+    discount_type,
+    discount_value,
+    valid_from,
+    valid_to,
+    per_user_limit,
+    usage_limit
+  });
+
+// check the creation 
+if(!promocode){
+  throw new BadRequestException('failed to create new promocode')
+}
+
+// save it
+    return this.repo.save(promocode);
   }
 
-  findAll() {
-    return `This action returns all promocode`;
+ async findAll() {
+    const promocodes = await this.repo.find();
+    if(!promocodes){
+      throw new NotFoundException('There is no Promocodes exist :(' )
+    }
+    return {
+      "Count: ": promocodes.length,
+      "Promocodes ": promocodes
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} promocode`;
+
+  async findOne(id: number) {
+    const promocode = await this.repo.findOne({where: {id}});
+    if(!promocode){
+      throw new NotFoundException(`There is no Promocode with this ID: ${id} exists :(` )
+    }
+    return promocode;
   }
 
-  update(id: number, updatePromocodeDto: UpdatePromocodeDto) {
-    return `This action updates a #${id} promocode`;
+  async update(id: number, updatePromocodeDto: UpdatePromocodeDto) {
+    const promocode = await this.findOne(id);
+    if(!promocode){
+      throw new NotFoundException(`There is no Promocode with this ID: ${id} exists :(` )
+    }
+    Object.assign(promocode, updatePromocodeDto);
+
+    return this.repo.save(promocode);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} promocode`;
+
+ async remove(id: number) {
+     const promocode = await this.findOne(id);
+    if(!promocode){
+      throw new NotFoundException(`There is no Promocode with this ID: ${id} exists :(` )
+    }
+
+    return this.repo.remove(promocode);
   }
 }
